652386462c9d2516c84cc3884394910b3703c2c8
kyle4913 <163797642+kyle4913@users.noreply.github.com>
2025-11-15 15:52:59 +0900

    commit_1
    first

---
+++ js/common.js
@@ -0,0 +1,439 @@
+$(function(){
+
+    // section divide
+    $('.w1').on('click',function(){
+        $('.p_w1').addClass('act')
+    })
+    $('.p_w1').on('click',function(){
+        $('.p_w1').removeClass('act')
+    })
+    // section divide
+    $('.w2').on('click',function(){
+        $('.p_w2').addClass('act')
+    })
+    $('.p_w2').on('click',function(){
+        $('.p_w2').removeClass('act')
+    })
+    // section divide
+    $('.w3').on('click',function(){
+        $('.p_w3').addClass('act')
+    })
+    $('.p_w3').on('click',function(){
+        $('.p_w3').removeClass('act')
+    })
+    // section divide
+    $('.w3').on('click',function(){
+        $('.p_w3').addClass('act')
+    })
+    $('.p_w3').on('click',function(){
+        $('.p_w3').removeClass('act')
+    })
+
+    // section divide
+    $('.w4').on('click',function(){
+        $('.p_w4').addClass('act')
+    })
+    $('.p_w4').on('click',function(){
+        $('.p_w4').removeClass('act')
+    })
+
+    // section divide
+    $('.w5').on('click',function(){
+        $('.p_w5').addClass('act')
+    })
+    $('.p_w5').on('click',function(){
+        $('.p_w5').removeClass('act')
+    })
+
+    // section divide
+    $('.w6').on('click',function(){
+        $('.p_w6').addClass('act')
+    })
+    $('.p_w6').on('click',function(){
+        $('.p_w6').removeClass('act')
+    })
+
+    // section divide
+    $('.w7').on('click',function(){
+        $('.p_w7').addClass('act')
+    })
+    $('.p_w7').on('click',function(){
+        $('.p_w7').removeClass('act')
+    })
+
+    // section divide
+    $('.w8').on('click',function(){
+        $('.p_w8').addClass('act')
+    })
+    $('.p_w8').on('click',function(){
+        $('.p_w8').removeClass('act')
+    })
+
+    // section divide
+    $('.w9').on('click',function(){
+        $('.p_w9').addClass('act')
+    })
+    $('.p_w9').on('click',function(){
+        $('.p_w9').removeClass('act')
+    })
+
+    // section divide
+    $('.w10').on('click',function(){
+        $('.p_w10').addClass('act')
+    })
+    $('.p_w10').on('click',function(){
+        $('.p_w10').removeClass('act')
+    })
+
+    // section divide
+    $('.w11').on('click',function(){
+        $('.p_w11').addClass('act')
+    })
+    $('.p_w11').on('click',function(){
+        $('.p_w11').removeClass('act')
+    })
+
+    // section divide
+    $('.w12').on('click',function(){
+        $('.p_w12').addClass('act')
+    })
+    $('.p_w12').on('click',function(){
+        $('.p_w12').removeClass('act')
+    })
+
+    // section divide
+    $('.w13').on('click',function(){
+        $('.p_w13').addClass('act')
+    })
+    $('.p_w13').on('click',function(){
+        $('.p_w13').removeClass('act')
+    })
+
+    // section divide
+    $('.w14').on('click',function(){
+        $('.p_w14').addClass('act')
+    })
+    $('.p_w14').on('click',function(){
+        $('.p_w14').removeClass('act')
+    })
+
+    // section divide
+    $('.w15').on('click',function(){
+        $('.p_w15').addClass('act')
+    })
+    $('.p_w15').on('click',function(){
+        $('.p_w15').removeClass('act')
+    })
+
+    // section divide
+    $('.w16').on('click',function(){
+        $('.p_w16').addClass('act')
+    })
+    $('.p_w16').on('click',function(){
+        $('.p_w16').removeClass('act')
+    })
+
+    // section divide
+    $('.w17').on('click',function(){
+        $('.p_w17').addClass('act')
+    })
+    $('.p_w17').on('click',function(){
+        $('.p_w17').removeClass('act')
+    })
+
+    // section divide
+    $('.w18').on('click',function(){
+        $('.p_w18').addClass('act')
+    })
+    $('.p_w18').on('click',function(){
+        $('.p_w18').removeClass('act')
+    })
+
+    // section divide
+    $('.w19').on('click',function(){
+        $('.p_w19').addClass('act')
+    })
+    $('.p_w19').on('click',function(){
+        $('.p_w19').removeClass('act')
+    })
+
+    // section divide
+    $('.w20').on('click',function(){
+        $('.p_w20').addClass('act')
+    })
+    $('.p_w20').on('click',function(){
+        $('.p_w20').removeClass('act')
+    })
+
+    // section divide
+    $('.w21').on('click',function(){
+        $('.p_w21').addClass('act')
+    })
+    $('.p_w21').on('click',function(){
+        $('.p_w21').removeClass('act')
+    })
+
+    // section divide
+    $('.w22').on('click',function(){
+        $('.p_w22').addClass('act')
+    })
+    $('.p_w22').on('click',function(){
+        $('.p_w22').removeClass('act')
+    })
+
+    // section divide
+    $('.w23').on('click',function(){
+        $('.p_w23').addClass('act')
+    })
+    $('.p_w23').on('click',function(){
+        $('.p_w23').removeClass('act')
+    })
+
+    // section divide
+    $('.w24').on('click',function(){
+        $('.p_w24').addClass('act')
+    })
+    $('.p_w24').on('click',function(){
+        $('.p_w24').removeClass('act')
+    })
+
+    // section divide
+    $('.w25').on('click',function(){
+        $('.p_w25').addClass('act')
+    })
+    $('.p_w25').on('click',function(){
+        $('.p_w25').removeClass('act')
+    })
+
+    // section divide
+    $('.w26').on('click',function(){
+        $('.p_w26').addClass('act')
+    })
+    $('.p_w26').on('click',function(){
+        $('.p_w26').removeClass('act')
+    })
+
+    // section divide
+    $('.w27').on('click',function(){
+        $('.p_w27').addClass('act')
+    })
+    $('.p_w27').on('click',function(){
+        $('.p_w27').removeClass('act')
+    })
+
+    // section divide
+    $('.w28').on('click',function(){
+        $('.p_w28').addClass('act')
+    })
+    $('.p_w28').on('click',function(){
+        $('.p_w28').removeClass('act')
+    })
+
+    // section divide
+    $('.w29').on('click',function(){
+        $('.p_w29').addClass('act')
+    })
+    $('.p_w29').on('click',function(){
+        $('.p_w29').removeClass('act')
+    })
+
+    // section divide
+    $('.w30').on('click',function(){
+        $('.p_w30').addClass('act')
+    })
+    $('.p_w30').on('click',function(){
+        $('.p_w30').removeClass('act')
+    })
+
+    // section divide
+    $('.w31').on('click',function(){
+        $('.p_w31').addClass('act')
+    })
+    $('.p_w31').on('click',function(){
+        $('.p_w31').removeClass('act')
+    })
+
+    // section divide
+    $('.w32').on('click',function(){
+        $('.p_w32').addClass('act')
+    })
+    $('.p_w32').on('click',function(){
+        $('.p_w32').removeClass('act')
+    })
+
+    // section divide
+    $('.w33').on('click',function(){
+        $('.p_w33').addClass('act')
+    })
+    $('.p_w33').on('click',function(){
+        $('.p_w33').removeClass('act')
+    })
+
+    // section divide
+    $('.w34').on('click',function(){
+        $('.p_w34').addClass('act')
+    })
+    $('.p_w34').on('click',function(){
+        $('.p_w34').removeClass('act')
+    })
+
+    // section divide
+    $('.w35').on('click',function(){
+        $('.p_w35').addClass('act')
+    })
+    $('.p_w35').on('click',function(){
+        $('.p_w35').removeClass('act')
+    })
+
+    // section divide
+    $('.w36').on('click',function(){
+        $('.p_w36').addClass('act')
+    })
+    $('.p_w36').on('click',function(){
+        $('.p_w36').removeClass('act')
+    })
+
+    // section divide
+    $('.w37').on('click',function(){
+        $('.p_w37').addClass('act')
+    })
+    $('.p_w37').on('click',function(){
+        $('.p_w37').removeClass('act')
+    })
+
+    // section divide
+    $('.w38').on('click',function(){
+        $('.p_w38').addClass('act')
+    })
+    $('.p_w38').on('click',function(){
+        $('.p_w38').removeClass('act')
+    })
+
+    // section divide
+    $('.w39').on('click',function(){
+        $('.p_w39').addClass('act')
+    })
+    $('.p_w39').on('click',function(){
+        $('.p_w39').removeClass('act')
+    })
+
+    // section divide
+    $('.w40').on('click',function(){
+        $('.p_w40').addClass('act')
+    })
+    $('.p_w40').on('click',function(){
+        $('.p_w40').removeClass('act')
+    })
+
+    // section divide
+    $('.w41').on('click',function(){
+        $('.p_w41').addClass('act')
+    })
+    $('.p_w41').on('click',function(){
+        $('.p_w41').removeClass('act')
+    })
+
+    // section divide
+    $('.w42').on('click',function(){
+        $('.p_w42').addClass('act')
+    })
+    $('.p_w42').on('click',function(){
+        $('.p_w42').removeClass('act')
+    })
+
+    // section divide
+    $('.w43').on('click',function(){
+        $('.p_w43').addClass('act')
+    })
+    $('.p_w43').on('click',function(){
+        $('.p_w43').removeClass('act')
+    })
+
+    // section divide
+    $('.w44').on('click',function(){
+        $('.p_w44').addClass('act')
+    })
+    $('.p_w44').on('click',function(){
+        $('.p_w44').removeClass('act')
+    })
+
+    // section divide
+    $('.w45').on('click',function(){
+        $('.p_w45').addClass('act')
+    })
+    $('.p_w45').on('click',function(){
+        $('.p_w45').removeClass('act')
+    })
+
+    // section divide
+    $('.w46').on('click',function(){
+        $('.p_w46').addClass('act')
+    })
+    $('.p_w46').on('click',function(){
+        $('.p_w46').removeClass('act')
+    })
+
+    // section divide
+    $('.w47').on('click',function(){
+        $('.p_w47').addClass('act')
+    })
+    $('.p_w47').on('click',function(){
+        $('.p_w47').removeClass('act')
+    })
+
+    // section divide
+    $('.w48').on('click',function(){
+        $('.p_w48').addClass('act')
+    })
+    $('.p_w48').on('click',function(){
+        $('.p_w48').removeClass('act')
+    })
+
+    // section divide
+    $('.w49').on('click',function(){
+        $('.p_w49').addClass('act')
+    })
+    $('.p_w49').on('click',function(){
+        $('.p_w49').removeClass('act')
+    })
+
+    // section divide
+    $('.w50').on('click',function(){
+        $('.p_w50').addClass('act')
+    })
+    $('.p_w50').on('click',function(){
+        $('.p_w50').removeClass('act')
+    })
+
+    // section divide
+    $('.w51').on('click',function(){
+        $('.p_w51').addClass('act')
+    })
+    $('.p_w51').on('click',function(){
+        $('.p_w51').removeClass('act')
+    })
+
+    // section divide
+    $('.w52').on('click',function(){
+        $('.p_w52').addClass('act')
+    })
+    $('.p_w52').on('click',function(){
+        $('.p_w52').removeClass('act')
+    })
+
+    // section divide
+    $('.w53').on('click',function(){
+        $('.p_w53').addClass('act')
+    })
+    $('.p_w53').on('click',function(){
+        $('.p_w53').removeClass('act')
+    })
+    // section divide
+    $('.w54').on('click',function(){
+        $('.p_w54').addClass('act')
+    })
+    $('.p_w54').on('click',function(){
+        $('.p_w54').removeClass('act')
+    })
+
+})
